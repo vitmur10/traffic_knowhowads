adfd35973262d2b1b21830b20a3fde5cdd4f05eb
Vitaliy Murovaniy <95306614+vitmur10@users.noreply.github.com>
2025-04-05 13:30:46 +0300

    done, sleep

---
+++ keyboard.py
@@ -0,0 +1,5 @@
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+
+order = InlineKeyboardMarkup(
+    inline_keyboard=[[InlineKeyboardButton(text="ЗАМОВИТИ ", url="https://t.me/yuliagobysh")],]
+)
